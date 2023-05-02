98a55827cfc0c2f5adeda86d7928226ac88eb543
alpdevelop <alkavas@hotmail.com>
2023-05-02 16:24:11 +0300

    liste özellikleri güncellendi liste tablo olarak güncellendi

--- Basket App/src/App.jsx
+++ Basket App/src/App.jsx
@@ -16,36 +16,42 @@ import "./App.css";
 
 const storeItems = [
   {
+    id: 100,
     name: "Futbol Topu",
     price: 50,
     img: "ball.jpg",
     desc: "Yüksek işçilik ve çok kaliteli malzemelerden yapılan bu futbol topu, hem amatör hem de profesyonel futbolcular için özenle tasarlanmıştır. Bu futbol topu, dayanıklı ve tam dairesel bir yapıya sahiptir ve herhangi bir sahada kullanılabilir.",
   },
   {
+    id: 101,
     name: "Spor Ayakkabı",
     price: 75,
     img: "shoes.jpg",
     desc: "Kaliteli ayakkabı, havalandırmalı tasarımı ile ayaklarınızın terlemesini önleyerek rahatlığı garanti eder. Alt tabanı, daha iyi tutunmayı sağlayarak yürüyüş sırasında ekstra destek sunar. Uzun ömürlü kullanımı ile kaliteyi bir arada sunar.",
   },
   {
+    id: 102,
     name: "Deri Cüzdan",
     price: 40,
     img: "wallet.jpg",
     desc: "Deri bir cüzdan, stil ve işlevselliği bir araya getiren bir aksesuardır. Üstün kaliteli deri malzemeden üretilmiştir ve dayanıklı bir yapıya sahiptir. Aynı zamanda şık bir görünüme sahiptir ve herhangi bir kıyafetle uyumlu olacak şekilde tasarlanmıştır.",
   },
   {
+    id: 103,
     name: "Klasik Saat",
     price: 35,
     img: "saat.jpg",
     desc: "Bu saat yüksek bir işçiliğe sahip ve kaliteli malzemelerden üretilmiştir. Uzun ömürlü bir kullanım sunar. Suya dayanıklı özellikleri ile de her türlü hava koşulunda kullanılabilir. Geçmişin klasik estetik anlayışıyla modern gereksinimleri, tarz sahibi ve kalite arayanlar için mükemmel bir seçimdir.",
   },
   {
+    id: 104,
     name: "Kar Küresi",
     price: 25,
     img: "kure.jpg",
     desc: "Estetik ve işlevsellik açısından öne çıkan ve İçindeki minik parçacıkların hareketiyle harika bir görsel sunan bu küre odanıza renk katar. Kaliteli malzemelerden üretilmiş ve dayanıklıdır. Her dekorasyon stiline uyum sağlayacak şekilde tasarlanmıştır.",
   },
   {
+    id: 105,
     name: "Kulaklık",
     price: 55,
     img: "kulaklık.jpg",
@@ -64,6 +70,19 @@ function App() {
       item.name.toLowerCase().indexOf(search) >= 0 ||
       item.price.toString().indexOf(search) >= 0
   );
+  let addFilterBasketProduct = ({ id, name, price, count = 1 }) => {
+    //id eşleştirmesi yapar
+    let itemIndex = basketItems.findIndex(item => item.id === id);
+    if (itemIndex >= 0) {
+      // Arrayi alıp, idsi eşleşenin index numarısındaki elemanın count'unu 1 arttırıyor.
+      let _basketItems = [...basketItems];
+      _basketItems[itemIndex].count += 1;
+      setbasketItems(_basketItems);
+    } else {
+      setbasketItems([...basketItems, { name, price, count, id }]); // diziye ekleme yapar
+    }
+  };
+
   return (
     <Container size="sm" px="xs" className="Store">
       <Grid grow gutter="xs" className="inputGrid">
@@ -98,7 +117,7 @@ function App() {
         </Indicator>
       </Grid>
       <SimpleGrid cols={3} spacing="xs" verticalSpacing="xs">
-        {filteredItems.map(({ name, price, img, desc }, i) => {
+        {filteredItems.map(({ name, price, img, desc, id }, i) => {
           return (
             <CardComponent
               key={i}
@@ -106,9 +125,7 @@ function App() {
               price={price}
               image={img}
               description={desc}
-              onAdd={() => {
-                setbasketItems([...basketItems, { name, price }]); // diziye ekleme yapar
-              }}
+              onAdd={() => addFilterBasketProduct({ name, price, id })}
             />
           );
         })}
--- Basket App/src/components/List.jsx
+++ Basket App/src/components/List.jsx
@@ -1,30 +1,56 @@
-import { List, ThemeIcon } from "@mantine/core";
+import { List, ThemeIcon, Badge, Table } from "@mantine/core";
 import { IconCircleCheck } from "@tabler/icons-react";
 
 // Gelen değere göre Listeleme yapar fiyat ve harfe göre
 
 function ListItems({ filterItem }) {
+  let filtredItem = filterItem.map(({ name, price, count }, i) => (
+    <tr key={i}>
+      <td>{name}</td>
+      <td>{price}</td>
+      <td>{count}</td>
+      <td>{count * price}</td>
+    </tr>
+  ));
+
   return (
-    <div className="List">
-      <List
-        spacing="sm"
-        size="sm"
-        center
-        icon={
-          <ThemeIcon color="teal" size={24} radius="xl">
-            <IconCircleCheck size="1rem" />
-          </ThemeIcon>
-        }>
-        {filterItem.map(({ name, price }, i) => (
-          <div className="listItem" key={i}>
-            <List.Item>
-              {name} {price} &#8378;
-            </List.Item>
-          </div>
-        ))}
-      </List>
-    </div>
+    <Table horizontalSpacing="md" striped>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Piece</th>
+          <th>Total Price </th>
+        </tr>
+      </thead>
+      <tbody>{filtredItem}</tbody>
+    </Table>
+    //     <div className="List">
+    // </div>
   );
 }
 
 export default ListItems;
+
+{
+  /* <List
+  spacing="sm"
+  size="sm"
+  center
+  icon={
+    <ThemeIcon color="teal" size={20} radius="xl">
+      <IconCircleCheck size="1rem" />
+    </ThemeIcon>
+  }>
+  {filterItem.map(({ name, price, count }, i) => (
+    <div className="listItem" key={i}>
+      <List.Item>
+        {count}
+        {name}
+        {price * count} &#8378;
+        <Badge className="listBadge">{price * count}</Badge>
+      </List.Item>
+    </div>
+  ))}
+</List> */
+}
